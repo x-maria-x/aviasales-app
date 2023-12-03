8250faf6e62595089ce8c5cac95a8b5ca295584a
x-maria-x <m_khvatova99@mail.ru>
2023-12-04 00:51:58 +0700

    hotfix

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-import logo from '../sources/img/Logo.svg'
+import logo from '../sources/img/Logo-.svg'
+import logoShadow from '../sources/img/Logo+.svg'
 
 import style from './logo.module.scss'
 
 function Logo() {
   const { stopFetching } = useSelector((state) => state.fetchData)
 
-  const styleClass = stopFetching ? `${style.logo}` : `${style.logo} ${style.spinner}`
-
-  return <img src={logo} alt="logo" className={styleClass} />
+  return stopFetching ? (
+    <img src={logoShadow} alt="logo" className={style.logo} />
+  ) : (
+    <img src={logo} alt="logo" className={`${style.logo} ${style.spinner}`} />
+  )
 }
 
 export default Logo
